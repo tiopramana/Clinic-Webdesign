30461e55c21c2e1a746e85767a62efaaee4bce81
Tio Pramana <tiopramanaa@gmail.com>
2026-01-01 17:39:52 +0800

    Completing Landing Page

--- script.js
+++ script.js
@@ -84,3 +84,20 @@ if (menuToggle && menuBar) {
 }
 
 Fancybox.bind("[data-fancybox]", {});
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      }
+    });
+  },
+  {
+    threshold: 0.15,
+  }
+);
+
+document.querySelectorAll(".fade").forEach((el) => {
+  observer.observe(el);
+});
